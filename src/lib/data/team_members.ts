439fe68ed026d87ab704ebc3b932dc94406109f6
Eugen1763 <finn1763@gmail.com>
2026-08-30 12:57:15 +0200

    feat: add Jannis to trainer team

--- src/lib/data/team_members.ts
+++ src/lib/data/team_members.ts
@@ -1,21 +1,27 @@
 ﻿export interface TeamMember {
-    name: string;
-    image: string;
-    description: string;
-    rank: string;
+	name: string;
+	image: string;
+	description: string;
+	rank: string;
 }
 
 export const teamMembers: TeamMember[] = [
-    {
-        name: "Maximilian Galle",
-        image: "optimized/max.webp",
-        description: "Inhaber und Head-Coach",
-        rank: "BJJ Black Belt",
-    },
-    {
-        name: "Marius Pichler",
-        image: "optimized/marius.webp",
-        description: "Inhaber",
-        rank: "BJJ Blue Belt"
-    }
+	{
+		name: 'Maximilian Galle',
+		image: 'optimized/max.webp',
+		description: 'Inhaber und Head-Coach',
+		rank: 'BJJ Black Belt'
+	},
+	{
+		name: 'Marius Pichler',
+		image: 'optimized/marius.webp',
+		description: 'Inhaber',
+		rank: 'BJJ Blue Belt'
+	},
+	{
+		name: 'Jannis Ohlerich',
+		image: 'optimized/jannis.webp',
+		description: 'Trainer',
+		rank: 'BJJ Purple Belt'
+	}
 ];
